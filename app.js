7857dfd76ff858f99590ed439fc622b70122fc0f
happy123livejp <57570263+happy123livejp@users.noreply.github.com>
2021-08-08 10:36:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Yoshito Hello World!'));
+app.get('/', (req, res) => res.send('Yoshito Hello World!Yoshito'));
 
-app.get('/', (req, res) => res.send('!'));
+app.get('/', (req, res) => res.send('Good moring!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
